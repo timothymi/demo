14b8a97f7c3a7bb69f63002b2cb0027e6223c558
timothymi <timothy@milliken-consultancy.com>
2021-06-08 17:54:05 +0100

    New project to test producer consumers

--- src/main/java/com/example/demo/KafkaConfiguration.java
+++ src/main/java/com/example/demo/KafkaConfiguration.java
@@ -12,6 +12,9 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +28,15 @@ public class KafkaConfiguration {
 		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+		configProps.put(ProducerConfig.CLIENT_ID_CONFIG, "cid1");
+		configProps.put("sasl.mechanism", SCRAM_SHA_256);
+		configProps.put("sasl.jass.config", devJassCfg);
+		configProps.put("security.protocol", SASL_PROTOCOL);
+		configProps.put("ssl.truststore.location", TRUSTSTORE_JKS);
+		configProps.put("ssl.truststore.password", "password");
+		configProps.put("ssl.endpoint.identification.algorithm", "");
 
 		return new DefaultKafkaProducerFactory<>(configProps);
 	}
@@ -41,6 +53,15 @@ public class KafkaConfiguration {
 		configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
 		configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		configProps.put(JsonDeserializer.TRUSTED_PACKAGES, TRUSTED_PACKAGE);
+		configProps.put("sasl.mechanism", SCRAM_SHA_256);
+		configProps.put("sasl.jass.config", consJaasCfg);
+		configProps.put("security.protocol", SASL_PROTOCOL);
+		configProps.put("ssl.truststore.location", TRUSTSTORE_JKS);
+		configProps.put("ssl.truststore.password", "password");
+		configProps.put("ssl.endpoint.identification.algorithm", "");
 
 		return new DefaultKafkaConsumerFactory<>(configProps);
 	}
