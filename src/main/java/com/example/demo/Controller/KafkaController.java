118922e2d15148151dd86a93c2a651e978d5a5c6
timothymi <timothy@milliken-consultancy.com>
2021-06-08 17:16:09 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.example.demo.Controller;
+
+import com.example.demo.service.ProducerService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public final class KafkaController {
+	private final ProducerService producerService;
+
+	public KafkaController(ProducerService producerService) {
+		this.producerService = producerService;
+	}
+
+	@PostMapping(value = "/publish")
+	public void sendMessageToKafkaTopic(@RequestParam String message) {
+		producerService.sendMessage(message);
+	}
+}
